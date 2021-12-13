9d98337dac10e85fabcd863d9a8a9549171aac02
Jarreau Fourie <jarreaufourie@outlook.com>
2021-12-13 20:05:28 +0200

    sidebar ui completed

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import SidebarLink from "./SidebarLink";
 import { HomeIcon } from "@heroicons/react/solid";
 import {
@@ -11,12 +10,13 @@ import {
 	DotsCircleHorizontalIcon,
 	DotsHorizontalIcon,
 } from "@heroicons/react/outline";
+import Image from "next/image";
 
 function Sidebar() {
 	return (
 		<div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
 			<div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
-				<Image src="https://rb.gy/ogau5a" width={30} height={30} />
+				<img src="https://rb.gy/ogau5a" width={30} height={30} />
 			</div>
 			<div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
 				<SidebarLink text="Home" Icon={HomeIcon} active />
@@ -27,6 +27,20 @@ function Sidebar() {
 				<SidebarLink text="Lists" Icon={ClipboardListIcon} />
 				<SidebarLink text="Profile" Icon={UserIcon} />
 				<SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+			</div>
+			<button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
+				Tweet
+			</button>
+			<div className="text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:ml-auto xl:-mr-5 mt-auto">
+				<img
+					src="https://rb.gy/ogau5a"
+					className="h-10 w-10 rounded-full xl:mr:2.5"
+				/>
+				<div className="hidden xl:inline leading-5">
+					<h4 className="font-bold">Firebase1234</h4>
+					<p className="text-[#6e767d]">Firebase1234</p>
+				</div>
+				<DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" />
 			</div>
 		</div>
 	);
